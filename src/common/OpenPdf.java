904e2f249c20476af529c1925aed1f0d3358a29f
PawanBahirat <99033177+PawanBahirat@users.noreply.github.com>
2022-02-05 10:47:24 +0530

    Add files via upload

---
+++ src/common/OpenPdf.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package common;
+
+import javax.swing.JOptionPane;
+import java.io.File;
+/**
+ *
+ * @author pawan
+ */
+public class OpenPdf {
+    public static void openById(String id){
+        try{
+            if((new File("C:\\Users\\pawan\\OneDrive\\Desktop\\CMS Bills\\"+id+".pdf")).exists()){
+            Process p = Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler C:\\Users\\pawan\\OneDrive\\Desktop\\CMS Bills\\"+id+".pdf");
+            }else{
+                JOptionPane.showMessageDialog(null, "File is not Exists");
+            }
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+}
